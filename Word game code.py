5e7f5fdd6fafc1ecb2d1ec90d0d6a2b284855f1d
jivepanda <smacleod9494@gmail.com>
2025-06-24 21:04:44 +0800

    fixed the while loop to  check length  and nod add it to the guess count

--- Word game code.py
+++ Word game code.py
@@ -1,5 +1,9 @@
 # word game for learning python
 import random
+
+from jinja2.nodes import Continue
+
+
 class Fruit:
     def __init__(self):
         self.words = ["apple", "banana", "cherry"]
@@ -26,29 +30,25 @@ AWORD = random.choice(random_category.words)
 
 is_active = True
 GUESSES = 2
+guess_count = 1
 print(f'''Welcome to Sam's word of the day 
 You have {GUESSES} guesses today's word has {len(AWORD)} letter and starts with {AWORD[0]} and is from the category {random_category.name}.''')
 #creating a loop for users to guess words
-UGuess = input(f'Guess the Word: ')
-while is_active:
+
+while is_active and guess_count <= GUESSES:
+    UGuess = input(f"Guess the {len(AWORD)}-letter word: ")
     if len(UGuess) != len(AWORD):
         print (f'That\'s not the right length Try agin with a {len(AWORD)} Word')
-        UGuess = input(f'Guess the Word Again: ')
-    else:
+       #UGuess = input(f'Guess the Word Again: ')
+        continue
+        guess_count += 1
+
+    if UGuess == AWORD:
+        print (f'''Checking .... 
+        Correct! YOU HAVE WON''')
         is_active = False
-guess_count = 1
-while guess_count <= GUESSES:
-    if UGuess != AWORD:
-        print (f'''Checking ....
-        Wrong Word Try again. 
-        Guess: ''', guess_count)
     else:
-        print('''Checking ....
-        Correct!''')
-        print ('YOU HAVE WON')
-        break
-    UGuess = input(f'Guess the Word Again: ')
-    guess_count += 1
+        print(f'''Checking ....
+        Wrong word. Guess attempt {guess_count} of {GUESSES}.''')
+
 print ('GAME OVER')
-
-
